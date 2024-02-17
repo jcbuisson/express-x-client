d14e29e8d3d737bf8385cff2618d741b4bf20feb
JC BUISSON <buisson@enseeiht.fr>
2024-02-17 06:38:06 +0100

    2.0.0

--- src/index.mjs
+++ src/index.mjs
@@ -1,102 +1,51 @@
- 
+
+function generateUID(length) {
+   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+   let uid = '';
+
+   for (let i = 0; i < length; i++) {
+     const randomIndex = Math.floor(Math.random() * characters.length)
+     uid += characters.charAt(randomIndex)
+   }
+   return uid
+}
+
+
 export default function expressXClient(socket, options={}) {
    if (options.debug === undefined) options.debug = false
    if (options.timeout === undefined) options.timeout = 5000
 
    const waitingPromisesByUid = {}
    const action2service2handlers = {}
-   const type2appHandlers = {}
-   let onConnectionCallback = null
-   let onReconnectionCallback = null
-   let onDisconnectionCallback = null
-   let nodeCnxId
+   const socketConnectionState = {}
 
-   const setConnectionCallback = (callback) => {
-      onConnectionCallback = callback
-   }
-
-   const setReconnectionCallback = (callback) => {
-      onReconnectionCallback = callback
-   }
-
-   const setDisconnectionCallback = (callback) => {
-      onDisconnectionCallback = callback
-   }
-
-   function _getCnxId() {
-      if (typeof sessionStorage !== 'undefined') return sessionStorage.getItem('expressx-cnx-id')
-      return nodeCnxId
-   }
-
-   function _setCnxId(id) {
-      if (typeof sessionStorage !== 'undefined') {
-         sessionStorage.setItem('expressx-cnx-id', id)
-      } else {
-         nodeCnxId = id
-      }
-   }
-
-   // on connection
-   socket.on("connected", async (connectionId) => {
-      if (options.debug) console.log('connected', connectionId)
-      // look for a previously stored connection id
-      const prevConnectionId = _getCnxId()
-      if (prevConnectionId) {
-         // it's a reconnection
-         if (prevConnectionId < 0) {
-            // ask server to transfer all data from connection `prevConnectionId` to connection `connectionId`
-            if (options.debug) console.log('cnx-transfer', -prevConnectionId, 'to', connectionId)
-            socket.emit('cnx-transfer', {
-               from: -prevConnectionId,
-               to: connectionId,
-            })
-            // set/update connection id
-            _setCnxId(connectionId)
-         } else {
-            if (options.debug) console.log('Error, previous connection id should be negative', prevConnectionId)
-         }
-
-      } else {
-         // set/update connection id
-         _setCnxId(connectionId)
-      }
-      // call user-defined connection callback
-      if (onConnectionCallback) onConnectionCallback(connectionId)
-   })
-
-   socket.on("cnx-transfer-ack", async (connection) => {
-      if (options.debug) console.log('cnx-transfer-ack', connection)
-      _setCnxId(connection.id)
-      // call user-defined reconnection callback
-      if (onReconnectionCallback) onReconnectionCallback(connection)
-   })
-
-
-   // A negative value for the connexion id means that the connection with the server has been lost
-   // Requests must wait until it goes positive again
-
-   // disconnection due to network issues
-   socket.on("disconnect", async (cause) => {
-      const id = _getCnxId()
-      if (id > 0) {
-         _setCnxId(-id)
-      } else {
-         if (options.debug) console.log('Error (disconnect), connection id should be negative', id)
+   socket.on("connect", async () => {
+      console.log("socket connected", socket.id)
+      if (socketConnectionState.resolve) {
+         socketConnectionState.resolve('ok')
+         socketConnectionState.status = 'connected'
       }
    })
 
-   // disconnection due to a page reload
-   if (typeof window !== 'undefined' && 'addEventListener' in window) {
-      window.addEventListener('unload', () => {
-         const id = _getCnxId()
-         if (id > 0) {
-            _setCnxId(-id)
-         } else {
-            if (options.debug) console.log('Error (unload), connection id should be negative', id)
-         }
+   socket.on("error", async () => {
+      console.log("socket connection error", socket.id)
+      if (socketConnectionState.reject) {
+         socketConnectionState.reject(err)
+         socketConnectionState.status = 'error'
+      }
+   })
+
+   async function socketConnection() {
+      const promise = new Promise((resolve, reject) => {
+         socketConnectionState.resolve = resolve
+         socketConnectionState.reject = reject
       })
+      return promise
    }
 
+   function socketStatus() {
+      return socketConnectionState.status
+   }
 
    // on receiving response from service request
    socket.on('client-response', ({ uid, error, result }) => {
@@ -127,21 +76,8 @@ export default function expressXClient(socket, options={}) {
       const handler = type2appHandlers[type]
       if (handler) handler(value)
    })
-
-   function wait(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-   }
-
+   
    async function serviceMethodRequest(name, action, ...args) {
-      // wait while stored connexion id is negative (= connection is lost with server)
-      let retries = 10
-      while (retries-- > 0) {
-         const id = _getCnxId()
-         if (id > 0) break
-         await wait(200)
-      }
-      if (retries === 0) throw new Error(`Timeout waiting for reconnection`)
-
       // create a promise which will resolve or reject by an event 'client-response'
       const uid = generateUID(20)
       const promise = new Promise((resolve, reject) => {
@@ -161,12 +97,6 @@ export default function expressXClient(socket, options={}) {
          args,
       })
       return promise
-   }
-
-   // define application events handlers
-   function on(type, handler) {
-      if (!type2appHandlers[type]) type2appHandlers[type] = {}
-      type2appHandlers[type] = handler
    }
 
    function service(name) {
@@ -192,22 +122,9 @@ export default function expressXClient(socket, options={}) {
    }
 
    return {
-      setConnectionCallback,
-      setReconnectionCallback,
-      setDisconnectionCallback,
+      socketConnection,
+      socketStatus,
       service,
-      on,
    }
 }
 
-
-function generateUID(length) {
-   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-   let uid = '';
-
-   for (let i = 0; i < length; i++) {
-     const randomIndex = Math.floor(Math.random() * characters.length)
-     uid += characters.charAt(randomIndex)
-   }
-   return uid
-}
